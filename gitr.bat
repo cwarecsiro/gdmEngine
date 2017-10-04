8dca03c0de319abd0ae4a8b9c2ab6d57b51b5458
Ware <war42q@csiro.au>
2017-10-04 15:15:34 +1100

    updating_all_files

--- gitr.bat
+++ gitr.bat
@@ -5,6 +5,7 @@ setlocal enabledelayedexpansion
 :: collect filepaths in variable
 set ARGS=%*
 
-echo %ARGS% > \\ces-10-cdc\\OSM_CDC_MMRG_work\\users\\bitbucket\\gdm_workflow\\argtest.txt
+:: used in trying to work out how to pass a white-space separated string as an arg across these envs
+::echo %ARGS% > \\ces-10-cdc\\OSM_CDC_MMRG_work\\users\\bitbucket\\gdm_workflow\\argtest.txt
 
 "C:\Program Files\Git\bin\sh" "//ces-10-cdc//OSM_CDC_MMRG_work//users//bitbucket//gdm_workflow//gitr.sh" %ARGS%
--- gitr.sh
+++ gitr.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
 cd $1
-#echo $1
-#echo $2
-#echo $3
 #echo "'$3'"
 git add $2
 #git commit -m "'$3'"
